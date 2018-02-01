e253bc837ce4493d1a417eed9c6bb26f0a1e7908
ohrha <trigramthree@gmail.com>
2018-02-01 05:28:28 -0500

    ADDED-PROCFILE-FOR-HEROKU

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,13 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(()=>{
+
+    if('serviceWorker' in navigator){
+
+      navigator.serviceWorker.register('/ngsw-worker.js');
+      alert("ITSINSTALLED")
+    }
+
+  })
   .catch(err => console.log(err));
