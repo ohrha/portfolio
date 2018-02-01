6454812d7ef4f660f19141c1078f9753c66ebe19
ohrha <trigramthree@gmail.com>
2018-02-01 03:46:27 -0500

    MOVE-@ANGULAR/CLI-AND-@ANGULAR/COMPILER-CLI-T0-DEPENDENCIES

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('*', (req,res)=>{
 
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '3000';
 const server = http.createServer(app);
 
 server.listen(port,"0.0.0.0",()=>console.log("Server Created at "+port))
