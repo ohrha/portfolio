fd4f0e45182f64de9e4ff742b07c2b214c64307c
ohrha <trigramthree@gmail.com>
2018-02-01 10:31:46 -0500

    ADDED-PROCFILE-FOR-HEROKU

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes, ActivatedRoute, Params } from '@angular/router';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
-import { environment } from '../environments/environment';
 
+import { environment } from '../environments/environment';
+import { RoseComponent } from './rose/rose.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes = [
+
+{ 
+  path:'rose', component:  RoseComponent
+}
+,{ 
+  path:'', component:  HomeComponent
+}
+//
+]
+///
+                           
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoseComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
