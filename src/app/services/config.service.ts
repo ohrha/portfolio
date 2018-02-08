33576686dfc41528e82a67257c1c962d7f12d854
ohrha <trigramthree@gmail.com>
2018-02-08 02:48:43 -0500

    WORK-ON-PUSH-NOTIFICATIONS-SWPUSH-CLASS

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment'
 @Injectable()
 export class ConfigService {
+
 private _config:any = environment.config
+
   constructor() { }
 
 get(key:any){
