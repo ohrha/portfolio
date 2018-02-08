57098449e55f00179818751492bd7c9e100eb7b4
ohrha <trigramthree@gmail.com>
2018-02-08 03:46:15 -0500

    WORK-ON-PUSH-NOTIFICATIONS-SWPUSH-CLASS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+   config:{
+    "VALID_PUBLIC_KEY":"BGgWbdrI76rpqXQXmgTGWsnYHCj0lXGfkpNp8up0TeVZBXuecerKE55gqKayH8soWE7aioU1MheuEZXFsp-hkIs"
+  }
 };
