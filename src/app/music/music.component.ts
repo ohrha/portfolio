524e288e2710ef9fc9e29877e80f62c822622e0b
ohrha <trigramthree@gmail.com>
2018-02-10 09:02:13 -0500

    'WORK-ON-PHOTOGRAPHY-SECTION

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -8,8 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class MusicComponent implements OnInit {
 
   constructor() { }
+  backgroundImageShow=true;
+  backgroundImageHide=false;
+  backgroundImageShow2 = false;
+  backgroundImageShow2Hide=false;
+  index=0;
 
   ngOnInit() {
+  }
+  changePhoto(){
+console.log("hello")
+    if(this.index==0){
+
+      this.backgroundImageShow=true;
+      this.backgroundImageShow2 = false;
+      this.index=1;
+      console.log(this.backgroundImageShow);
+      console.log(this.index)
+      //this.backgroundImageHide = true;
+
+    }
+    else if(this.index == 1){
+
+      this.backgroundImageShow2 = true;
+      this.backgroundImageShow = false;
+      this.index=0
+      console.log(this.backgroundImageShow2);
+      console.log(this.index)
+    }
+
   }
 
 }
