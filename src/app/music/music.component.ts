bb74ede1c96fce00c324720dcd27f8255a47164c
ohrha <trigramthree@gmail.com>
2018-02-10 10:01:47 -0500

    'WORK-ON-PHOTOGRAPHY-SECTION

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MusicComponent implements OnInit {
 
   constructor() { }
-  backgroundImageShow=true;
+  backgroundImageShow=false;
   backgroundImageHide=false;
   backgroundImageShow2 = false;
   backgroundImageShow2Hide=false;
