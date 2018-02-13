6892e9637bacbcf77439a1acc9c556d67f88f994
ohrha <trigramthree@gmail.com>
2018-02-13 00:57:55 -0500

    WORK-ON-ROSE-PAGE

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -8,90 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class MusicComponent implements OnInit {
 
   constructor() { }
-  backgroundImageShow=false;
-  backgroundImageHide=false;
-  backgroundImageShow2 = false;
-  backgroundImageShow3 = false;
-  backgroundImageShow4 = false;
-  backgroundImageShow5 = false;
-  backgroundImageShow6 = false;
-  backgroundImageShow7 = false;
-  
-  backgroundImageShow2Hide=false;
-  index=4;
+
+
 
   ngOnInit() {
   }
-  changePhoto(){
-console.log("hello")
-    if(this.index==0){
 
-      this.backgroundImageShow=true;
-      this.backgroundImageShow2 = false;
-      this.index=1;
-      console.log(this.backgroundImageShow);
-      console.log(this.index)
-      //this.backgroundImageHide = true;
-
-    }
-    else if(this.index == 1){
-
-      this.backgroundImageShow2 = true;
-      this.backgroundImageShow = false;
-      this.index=2
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-    }
-       else if(this.index == 2){
-
-      this.backgroundImageShow3 = true;
-      this.backgroundImageShow2 = false;
-      this.index=3
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-    }
-       else if(this.index == 3){
-
-      this.backgroundImageShow4 = true;
-      this.backgroundImageShow3 = false;
-      this.index=4
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-    }
-       else if(this.index == 4){
-
-      this.backgroundImageShow5 = true;
-      this.backgroundImageShow4 = false;
-      this.index=5
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-    }
-       else if(this.index == 5){
-
-      this.backgroundImageShow6 = true;
-      this.backgroundImageShow5 = false;
-      this.index=6
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-    }
-       else if(this.index == 6){
-
-      this.backgroundImageShow7 = true;
-      this.backgroundImageShow6 = false;
-      this.index=7
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-
-    }
-           else if(this.index == 7){
-     //this.backgroundImageShow = true;
-      this.backgroundImageShow7 = false;
-      this.index=0
-      console.log(this.backgroundImageShow2);
-      console.log(this.index)
-    }////////////////////////
-         
-
-  }
 
 }
--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -9,7 +9,77 @@ export class PhotographyComponent implements OnInit {
 
   constructor() { }
 
+  backgroundImageShow=false;
+  backgroundImageHide=false;
+  backgroundImageShow2 = false;
+  backgroundImageShow3 = false;
+  backgroundImageShow4 = false;
+  backgroundImageShow5 = false;
+  backgroundImageShow6 = false;
+  backgroundImageShow7 = false;
+  backgroundImageShow2Hide=false;
+  index:number = 0;
+
   ngOnInit() {
+  }
+   changePhoto(){
+
+    if(this.index==0){
+
+      this.backgroundImageShow=true;
+      this.backgroundImageShow2 = false;
+      this.index=1;
+
+    }
+    else if(this.index == 1){
+
+      this.backgroundImageShow2 = true;
+      this.backgroundImageShow = false;
+      this.index=2
+
+    }
+       else if(this.index == 2){
+
+      this.backgroundImageShow3 = true;
+      this.backgroundImageShow2 = false;
+      this.index=3
+
+    }
+       else if(this.index == 3){
+
+      this.backgroundImageShow4 = true;
+      this.backgroundImageShow3 = false;
+      this.index=4
+
+    }
+       else if(this.index == 4){
+
+      this.backgroundImageShow5 = true;
+      this.backgroundImageShow4 = false;
+      this.index=5
+
+    }
+       else if(this.index == 5){
+
+      this.backgroundImageShow6 = true;
+      this.backgroundImageShow5 = false;
+      this.index=6
+
+    }
+       else if(this.index == 6){
+
+      this.backgroundImageShow7 = true;
+      this.backgroundImageShow6 = false;
+      this.index=7
+
+    }
+      else if(this.index == 7){
+      this.backgroundImageShow7 = false;
+      this.index=0
+
+    }
+         
+
   }
 
 }
